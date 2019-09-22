44498989779bb30f62a4b38d26a4dd376cedae84
Alex <bahx4pc@gmail.com>
2019-09-22 14:09:25 +0200

    add comments; refactoring

--- source/md5_hash.h
+++ source/md5_hash.h
@@ -3,13 +3,19 @@
 #include <string>
 #include <array>
 
+// info and algorithm: https://en.wikipedia.org/wiki/MD5
 class md5_hash
 {
 public:
-	[[nodiscard]] static std::array<uint8_t, 16> compute(const std::string & message)
+	
+	/**
+	 * \param message message to processing
+	 * \return get md5-hash from message
+	 */
+	[[nodiscard]] static std::array<uint8_t, 16> compute(const std::string& message)
 	{
 		// These vars will contain the hash
-		uint32_t h0 = 0x67452301, h1 = 0xefcdab89, h2 = 0x98badcfe, h3 = 0x10325476;
+		uint32_t a0 = 0x67452301, b0 = 0xefcdab89, c0 = 0x98badcfe, d0 = 0x10325476;
 		size_t new_len = message.size() + 1;
 
 		//Pre-processing:
@@ -30,28 +36,33 @@ public:
 		for (size_t i = new_len + 4; i < new_len + 8; i++)
 			msg_copy[i] = bytes[i - new_len - 4];
 
+		// process the message in successive 512-bit chunks
 		for (size_t i = 0; i < new_len; i += (512 / 8))
 		{
 			uint32_t w[16];
+			
 			for (size_t j = 0; j < 16; j++)
 			{
-				const std::array<uint8_t, 4> array
-				{
-					static_cast<unsigned char>(msg_copy[i + j * 4]),
-					static_cast<unsigned char>(msg_copy[i + j * 4 + 1]),
-					static_cast<unsigned char>(msg_copy[i + j * 4 + 2]),
-					static_cast<unsigned char>(msg_copy[i + j * 4 + 3]),
+				const std::array<uint8_t, 4> array {
+					static_cast<uint8_t>(msg_copy[i + j * 4]),
+					static_cast<uint8_t>(msg_copy[i + j * 4 + 1]),
+					static_cast<uint8_t>(msg_copy[i + j * 4 + 2]),
+					static_cast<uint8_t>(msg_copy[i + j * 4 + 3]),
 				};
 
 				w[j] = md5_hash::uint32_from_4_bytes(array);
 			}
 
-			uint32_t a = h0, b = h1, c = h2, d = h3;
+			uint32_t a = a0, b = b0, c = c0, d = d0;
 
 			for (size_t j = 0; j < 64; j++)
 			{
 				uint32_t f, g;
 
+				// F(B, C, D)= (B and C) or (not B and D)
+				// G(B, C, D)= (B and D) or (C and not D)
+				// H(B, C, D)= B xor C xor D
+				// I(B, C, D)= C xor (B or not D)
 				if (j < 16)
 				{
 					f = (b & c) | ((~b) & d);
@@ -59,7 +70,7 @@ public:
 				}
 				else if (j < 32)
 				{
-					f = (d & b) | ((~d) & c);
+					f = (d & b) | (c & (~d));
 					g = (5 * j + 1) % 16;
 				}
 				else if (j < 48)
@@ -79,38 +90,40 @@ public:
 				b = b + md5_hash::rotate_left((a + f + md5_hash::k[j] + w[g]), md5_hash::r[j]);
 				a = temp;
 			}
-
-			h0 += a;
-			h1 += b;
-			h2 += c;
-			h3 += d;
+			
+			// add this chunk's hash to result so far:
+			a0 += a;
+			b0 += b;
+			c0 += c;
+			d0 += d;
 		}
 
-		std::array<uint8_t, 4> h0_arr = md5_hash::uint32_to_4_bytes(h0);
-		std::array<uint8_t, 4> h1_arr = md5_hash::uint32_to_4_bytes(h1);
-		std::array<uint8_t, 4> h2_arr = md5_hash::uint32_to_4_bytes(h2);
-		std::array<uint8_t, 4> h3_arr = md5_hash::uint32_to_4_bytes(h3);
+		const std::array<uint8_t, 4> a0_arr = md5_hash::uint32_to_4_bytes(a0);
+		const std::array<uint8_t, 4> b0_arr = md5_hash::uint32_to_4_bytes(b0);
+		const std::array<uint8_t, 4> c0_arr = md5_hash::uint32_to_4_bytes(c0);
+		const std::array<uint8_t, 4> d0_arr = md5_hash::uint32_to_4_bytes(d0);
 
+		// append results bytes
 		const std::array<uint8_t, 16> result = {
-			h0_arr[0],
-			h0_arr[1],
-			h0_arr[2],
-			h0_arr[3],
+			a0_arr[0],
+			a0_arr[1],
+			a0_arr[2],
+			a0_arr[3],
 
-			h1_arr[0],
-			h1_arr[1],
-			h1_arr[2],
-			h1_arr[3],
+			b0_arr[0],
+			b0_arr[1],
+			b0_arr[2],
+			b0_arr[3],
 
-			h2_arr[0],
-			h2_arr[1],
-			h2_arr[2],
-			h2_arr[3],
+			c0_arr[0],
+			c0_arr[1],
+			c0_arr[2],
+			c0_arr[3],
 
-			static_cast<uint8_t>(h3_arr[0]),
-			static_cast<uint8_t>(h3_arr[1]),
-			static_cast<uint8_t>(h3_arr[2]),
-			static_cast<uint8_t>(h3_arr[3]),
+			d0_arr[0],
+			d0_arr[1],
+			d0_arr[2],
+			d0_arr[3],
 		};
 
 		return result;
@@ -118,11 +131,13 @@ public:
 private:
 	md5_hash() = default;
 
-	static uint32_t rotate_left(uint32_t x, int n)
+	// rol command: https://www.aldeid.com/wiki/X86-assembly/Instructions/rol
+	static uint32_t rotate_left(const uint32_t x, const int32_t n)
 	{
 		return (x << n) | (x >> (32 - n));
 	}
 
+	// cast unsigned integer to to a four-element array
 	static std::array<uint8_t, 4> uint32_to_4_bytes(uint32_t value)
 	{
 		std::array<uint8_t, 4> bytes{};
@@ -135,6 +150,7 @@ private:
 		return bytes;
 	}
 
+	// cast a four-element array to unsigned integer
 	static uint32_t uint32_from_4_bytes(std::array<uint8_t, 4> bytes)
 	{
 		uint32_t value = 0;
@@ -147,6 +163,11 @@ private:
 		return value;
 	}
 
+	// table, computed by this code:
+	/* 
+		for i from 0 to 63
+			K[i] : = floor(232 × abs(sin(i + 1)))
+	*/
 	constexpr static uint32_t k[64] = {
 		0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee ,
 		0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 ,
@@ -166,6 +187,7 @@ private:
 		0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
 	};
 
+	// specifies the per-round shift amounts
 	constexpr static uint32_t r[64] = {
 		7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
 		5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
